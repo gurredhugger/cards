118d2496dc332a90ce75393f18c6bb9854bad0da
Yaroslav Sukhanek <ya.suhanek@ya.ru>
2019-08-26 12:54:06 +0300

    first commit

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=cards',
+    'username' => 'yii2admin',
+    'password' => 'parol6**',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
